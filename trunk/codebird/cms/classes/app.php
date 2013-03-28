91b6b1d018083a51f52a50d1786b1a8eba2a6def
pavlenkoia@gmail.com <pavlenkoia@gmail.com@31282947-cd2d-c87a-dc0e-155abdcf472b>
2013-03-28 07:12:05 +0000

    Сделана буферезация вывода, в связи с этим отложеные функции и редирект

---
+++ trunk/codebird/cms/classes/app.php
@@ -0,0 +1,145 @@
+<?php
+/**
+* Класс приложения
+*/
+class App
+{
+    /**
+     * Возвращает регистр
+     */
+    public static function GetRegistry()
+    {
+        return Registry::__instance();
+    }
+
+    /**
+     * Старт буферизации страницы
+     */
+    public static function StartBuffer()
+    {
+        $registry = Registry::__instance();
+
+        $registry->AppBuffer = array();
+
+        $registry->AppProperty = array();
+
+        ob_start();
+
+    }
+
+    /**
+     * Выводит отложеное свойство
+     *
+     * @param $name имя свойства
+     */
+    public static function ShowProperty($name)
+    {
+        $registry = Registry::__instance();
+
+        $appBuffer = $registry->AppBuffer;
+
+        $appBuffer[] = ob_get_clean();
+
+        $appBuffer[] = array('name'=>$name);
+
+        $registry->AppBuffer = $appBuffer;
+
+        ob_start();
+    }
+
+    /**
+     * Устанавливает отложеное свойство
+     *
+     * @param $name имя свойства
+     * @param $value значение свойства
+     */
+    public static function SetProperty($name, $value)
+    {
+        $registry = Registry::__instance();
+
+        $appProperty = $registry->AppProperty;
+
+        $appProperty[$name] = $value;
+
+        $registry->AppProperty = $appProperty;
+    }
+
+    /**
+     * Получить значение отложеного свойства
+     *
+     * @param $name имя свойства
+     */
+    public static function GetProperty($name)
+    {
+        $registry = Registry::__instance();
+
+        $appProperty = $registry->AppProperty;
+
+        if(isset($appProperty[$name]))
+        {
+            return $appProperty[$name];
+        }
+
+        return null;
+    }
+
+    /**
+     * Вывод буфера
+     */
+    public static function OutBuffer()
+    {
+        $registry = Registry::__instance();
+
+        $appProperty = $registry->AppProperty;
+
+        $appBuffer = $registry->AppBuffer;
+
+        $appBuffer[] = ob_get_clean();
+
+        $content = '';
+
+        foreach($appBuffer as $buffer)
+        {
+            if(is_array($buffer))
+            {
+                $name = $buffer['name'];
+                if(isset($appProperty[$name]))
+                {
+                    $content .= $appProperty[$name];
+                }
+            }
+            else
+            {
+                $content .= $buffer;
+            }
+        }
+
+        $registry->remove('AppBuffer');
+
+        echo $content;
+    }
+
+    public static function Redirect($url)
+    {
+        if(preg_match("'^(http://|https://|ftp://)'i", $url))
+        {
+            header("Request-URI: ".$url);
+            header("Content-Location: ".$url);
+            header("Location: ".$url);
+        }
+        else
+        {
+            $host = $_SERVER['HTTP_HOST'];
+            if($_SERVER['SERVER_PORT'] <> 80 && $_SERVER['SERVER_PORT'] <> 443 && $_SERVER['SERVER_PORT'] > 0 && strpos($_SERVER['HTTP_HOST'], ":") === false)
+                $host .= ":".$_SERVER['SERVER_PORT'];
+
+            $protocol = "http";
+
+            header("Request-URI: ".$protocol."://".$host.$url);
+            header("Content-Location: ".$protocol."://".$host.$url);
+            header("Location: ".$protocol."://".$host.$url);
+        }
+
+        exit;
+    }
+}
