e6eaebe1ff27eabc35d66ff370ee2a78e7b0b589
pavlenkoia@gmail.com <pavlenkoia@gmail.com@31282947-cd2d-c87a-dc0e-155abdcf472b>
2013-04-05 12:12:02 +0000

    Модуль sphider-utf

--- trunk/codebird/cms/modules/search/views/admin.reindex.php
+++ trunk/codebird/cms/modules/search/views/admin.reindex.php
@@ -1,8 +1,8 @@
 <?php
 
-$soption = 'full';
-$reindex = 1;
-$url = $domain;
+$_POST['soption'] = 'full';
+$_POST['reindex'] = 1;
+$_POST['url'] = "http://".$domain."/";
 
 
 include(SITE_PATH."/cms/modules/search/html/sphider-utf/admin/spider.php");
