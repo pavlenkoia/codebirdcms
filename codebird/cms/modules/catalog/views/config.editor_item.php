a3185d44adb3499702cf227c239e938c8c78e0b5
pavlenkoia@gmail.com <pavlenkoia@gmail.com@31282947-cd2d-c87a-dc0e-155abdcf472b>
2013-04-19 07:51:30 +0000

    Конфигуратор

--- codebird/cms/modules/catalog/views/config.editor_item.php
+++ codebird/cms/modules/catalog/views/config.editor_item.php
@@ -1,5 +1,5 @@
 {
-    xtype: 'panel',
+    xtype: 'form',
     frame: true,
     bodyBorder : true,
     autoScroll: true,
@@ -9,7 +9,105 @@
     items:
     [
         {
+            xtype:'fieldset',
+            title: <?=escapeJSON(($is_position)?'Позиции':'Раздел')?>,
+            collapsible: false,
+            autoHeight:true,
+            items:
+            [
+                {
+                    xtype: 'displayfield',
+                    fieldLabel: 'Название',
+                    value: <?=escapeJSON($table['title'])?>
+                },
+                {
+                    xtype: 'displayfield',
+                    fieldLabel: 'Таблица',
+                    value: <?=escapeJSON('`'.$table['table'].'`')?>
+                }
+            ]
+        },
+        {
+            xtype:'fieldset',
+            title: <?=escapeJSON('Поля таблицы `'.$table['table'].'`')?>,
+            collapsible: false,
+            autoHeight:true,
+            items:
+            [
+                {
+                    xtype:'grid',
+                    frame: false,
+                    colModel: new Ext.grid.ColumnModel
+                    ({
+                        defaults:
+                        {
+                            width: 120
+                        },
+                        columns:
+                        [
+                            {header: 'Имя', dataIndex: 'Field',width:240},
+                            {header: 'Тип', dataIndex: 'Type',width:240}
+                        ]
+                    }),
+                    listeners:
+                    {
+                        render: function()
+                        {
+                            var ds = new Ext.data.Store
+                            ({
+                                url: '/ajax/cm/catalog.config.fields_records',
+                                data:
+                                {
+                                    table_name: <?=escapeJSON($table['table'])?>
+                                },
+                                reader: new Ext.data.JsonReader
+                                ({
+                                    autoDestroy: true,
+                                    idProperty: 'Field',
+                                    root: 'rows',
+                                    fields:
+                                    [
+                                        'Field',
+                                        'Type'
+                                    ]
+                                })
+                            });
+                            //this.setStore(ds);
+                            //this.store = ds;
+                            //this.getStore().load();
+                        }
+                    }//,
+                    //sm: new Ext.grid.CheckboxSelectionModel({}),
+                    /*store: new Ext.data.Store
+                    ({
+                        url: '/ajax/cm/catalog.config.fields_records',
+                        data:
+                        {
+                            table_name: <?=escapeJSON($table['table'])?>
+                        },
+                        reader: new Ext.data.JsonReader
+                        ({
+                            autoDestroy: true,
+                            idProperty: 'Field',
+                            root: 'rows',
+                            fields:
+                            [
+                                'Field',
+                                'Type'
+                            ]
+                        })
+                    })*/
+                }
+            ]
+        },
+        {
             html: <?=escapeJSON('<pre>'.print_r($param_table,1).'</pre>');?>
+        },
+        {
+            html: <?=escapeJSON('<pre>'.print_r($table,1).'</pre>');?>
+        },
+        {
+            html: <?=escapeJSON('<pre>'.print_r($db_fields,1).'</pre>');?>
         }
     ]
 }
