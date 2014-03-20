b82b7795e0e94c8507aa59723a6fca5e385955d1
pavlenkoia@gmail.com <pavlenkoia@gmail.com@31282947-cd2d-c87a-dc0e-155abdcf472b>
2014-03-20 06:46:38 +0000

    ПОСЧИТАТЬ РАЗНИЦУ МЕЖДУ TIMESTAMP НА PHP

--- codebird/cms/classes/langvars.php
+++ codebird/cms/classes/langvars.php
@@ -60,5 +60,50 @@
       {
         return self::replaceMonth(date('m',$datestamp));
       }
+
+
+      /**
+       * Вычисляем разницу между двумя timestamps
+       * Параметры:
+       *   string $date_start начальная дата в формате unix timestamp
+       *   string $date_finish конечая дата в формате unix timestamp
+       * Возвращает
+       *   массив в следующем формате: 0 => секунды, 1 =>	минуты, 2 => часы, 3 => дни, 4 => месяцы, 5 => годы
+       * @author zerkms (Ivan Kurnosov)
+       */
+
+      public static function calcPeriod($date_start, $date_finish) {
+          $st = explode('-', date('d-m-Y-H-i-s', $date_start));
+          $fin = explode('-', date('d-m-Y-H-i-s', $date_finish));
+
+          if (($seconds = $fin[5] - $st[5]) < 0) {
+              $fin[4]--;
+              $seconds += 60;
+          }
+
+          if (($minutes = $fin[4] - $st[4]) < 0) {
+              $fin[3]--;
+              $minutes += 60;
+          }
+
+          if (($hours = $fin[3] - $st[3]) < 0) {
+              $fin[0]--;
+              $hours += 24;
+          }
+
+          if (($days = $fin[0] - $st[0]) < 0) {
+              $fin[1]--;
+              $days = date('t', mktime(1, 0, 0, $st[1], $st[0], $st[2])) - $st[0] + $fin[0];
+          }
+
+          if (($months = $fin[1] - $st[1]) < 0) {
+              $fin[2]--;
+              $months += 12;
+          }
+
+          $years = $fin[2] - $st[2];
+
+          return array($seconds, $minutes, $hours, $days, $months, $years);
+      }
   
   }
